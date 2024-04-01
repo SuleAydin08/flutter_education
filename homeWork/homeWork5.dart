8f4f8cea91dfbf9ce2bd7e4037c310da3cc40f90
saydin <saydin@tederga.com>
2024-04-01 08:56:37 +0300

    [ + ] Home Work 5-1 soru

---
+++ homeWork/homeWork5.dart
@@ -0,0 +1,19 @@
+//1-)Bir harita (map) yapısı kullanarak, bir öğrenci not defteri oluşturun. 
+//Anahtarlar öğrenci adını, değerler ise öğrencinin ders notlarını içermelidir. 
+//Kullanıcıya, belirli bir öğrencinin ders notlarını almak için bir sorgu yapabilen bir işlev tasarlayın.--------------------------------------------------
+
+// void main() {
+//   Map <String, int> notes = {
+//     'Şule':90,
+//     'Gizem':95,
+//     'Can':85,
+//     'Hale':90,
+//     'Nur':95,
+//     'Onur':85,
+//   };
+//   print(notes['Nur']);
+// }//95
+
+//2-)Bir listedeki tüm pozitif tam sayıları çıkararak, kalan negatif tam sayıları yeni bir listede saklayan bir Dart işlevi yazın. 
+//Örneğin, [1, -2, 3, -4, 5, -6] gibi bir liste verildiğinde, işlev [-2, -4, -6] gibi bir çıktı döndürmelidir.-------------------------------------------------
+
