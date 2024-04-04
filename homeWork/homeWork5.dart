b0739ace211edbe8b7070b365f8b7c1865250bcc
saydin <saydin@tederga.com>
2024-04-04 09:46:31 +0300

    [ + ] Yapabildiğim ödevler ektedir. Söylediğin value getirmeye evde bakacağım.

--- homeWork/homeWork5.dart
+++ homeWork/homeWork5.dart
@@ -11,9 +11,30 @@
 //     'Nur':95,
 //     'Onur':85,
 //   };
-//   print(notes['Nur']);
+//   print(notes['Nur']);//Hepsini görüntüleyeceksek ayrı ayrı printliyoruz.
 // }//95
 
 //2-)Bir listedeki tüm pozitif tam sayıları çıkararak, kalan negatif tam sayıları yeni bir listede saklayan bir Dart işlevi yazın. 
 //Örneğin, [1, -2, 3, -4, 5, -6] gibi bir liste verildiğinde, işlev [-2, -4, -6] gibi bir çıktı döndürmelidir.-------------------------------------------------
+
+//3-)Bir harita(map) yapısı kullanarak, bir restoranın menüsünü temsil edin.Anahtarlar yemek adını ,değerler ise yemek fiyatını içermelidir.Kullanıcıya, belirli bir yemeğin fiyatını almak için bir sorgu yapabilen bir işlev tasarlayın.Ayrıca restorandaki en pahalı yemeği bulan bir işlevde ekleyin.
+
+// void main() {
+//  Map <String, int> food = {"Mercimek çorbası":100,
+//                           "Adana kebap":200,
+//                           "Urfa kebap":190,
+//                           ":Beyti":220};
+//   print(food["Adana kebap"]);
+// }//200
+
+//4-Bir harita map yapısı kullanarak ,bir şehirdeki otelleri temsil edin. Anahtarlar otel adını,değerler ise otelin konumunu ve yıldız sayısını içermelidir. Kullanıcıya belirli bir otelin konumunu ve yıldız sayısını almak için bir sorgu yapabilen bir işlev tasarlayın .Ayrıca,belirli bir yıldız sayısına sahip tüm otelleri listelemek için bir işlev de ekleyin.
+
+// void main() {
+//   Map<String, int> hotel = {
+//     "Luxes Otel": 5,
+//     "Darwin Otel": 4,
+//     "Nazende Otel": 3,
+//   };
+//   print(hotel["Luxes Otel"]);
+// }//5
 
