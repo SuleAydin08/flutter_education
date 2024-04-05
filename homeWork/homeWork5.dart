dd2f7a6f1bad9713e72528faa543f5c6d859e62b
saydin <saydin@tederga.com>
2024-04-05 09:27:56 +0300

    [ + ] 2 soru eklenmiştir. [ + ] 3 soru eklenmiştir. [ + ] 4 de hata çıkan sorudur.

--- homeWork/homeWork5.dart
+++ homeWork/homeWork5.dart
@@ -17,24 +17,66 @@
 //2-)Bir listedeki tüm pozitif tam sayıları çıkararak, kalan negatif tam sayıları yeni bir listede saklayan bir Dart işlevi yazın. 
 //Örneğin, [1, -2, 3, -4, 5, -6] gibi bir liste verildiğinde, işlev [-2, -4, -6] gibi bir çıktı döndürmelidir.-------------------------------------------------
 
-//3-)Bir harita(map) yapısı kullanarak, bir restoranın menüsünü temsil edin.Anahtarlar yemek adını ,değerler ise yemek fiyatını içermelidir.Kullanıcıya, belirli bir yemeğin fiyatını almak için bir sorgu yapabilen bir işlev tasarlayın.Ayrıca restorandaki en pahalı yemeği bulan bir işlevde ekleyin.
+// void main() {
+//   List<int> numbers = [1,-1,2,-2,3,-3,4,-4,5,-5,6,-6];
+//   var evenNumbers = numbers.where((number) => number < 0).toList();//Listedeki 0 küçük elemanları getir ve bunu listeye çevirerek getir demek .
+//   print(evenNumbers);
+// }//[-1, -2, -3, -4, -5, -6]
+
+//3-)Bir harita(map) yapısı kullanarak, bir restoranın menüsünü temsil edin.Anahtarlar yemek adını ,değerler ise yemek fiyatını içermelidir.
+//Kullanıcıya, belirli bir yemeğin fiyatını almak için bir sorgu yapabilen bir işlev tasarlayın.
+//Ayrıca restorandaki en pahalı yemeği bulan bir işlevde ekleyin.------------------------------------------------------------------
 
 // void main() {
-//  Map <String, int> food = {"Mercimek çorbası":100,
+//  Map <String, int> foods = {"Mercimek çorbası":100,
 //                           "Adana kebap":200,
 //                           "Urfa kebap":190,
-//                           ":Beyti":220};
-//   print(food["Adana kebap"]);
+//                           "Beyti":220};
+//   print(foods["Adana kebap"]);//Burda istediğimiz yemeğin fiyatını görüntülemek için çağırma işlemi yapıyoruz.
 // }//200
 
-//4-Bir harita map yapısı kullanarak ,bir şehirdeki otelleri temsil edin. Anahtarlar otel adını,değerler ise otelin konumunu ve yıldız sayısını içermelidir. Kullanıcıya belirli bir otelin konumunu ve yıldız sayısını almak için bir sorgu yapabilen bir işlev tasarlayın .Ayrıca,belirli bir yıldız sayısına sahip tüm otelleri listelemek için bir işlev de ekleyin.
+// void main() {
+//  String? highestValue;
+//   Map<Object? , Object?> ? foods = {
+//     "Mercimek Çorbası":100,
+//     "Adana Kebap":200,
+//     "Urfa Kebap":190,
+//     "Beyti":220
+//   };
+//   highestValue = foods.entries.reduce((x,y) {
+//     final xValue = x.value;
+//     final yValue = y.value;
+//     if(xValue is! int){
+//       return y;
+//     }
+//     if(yValue is! int){
+//       return x;
+//     }
+//     return xValue > yValue ? x : y;
+//   }).key as String?;
+//   print(highestValue);
+// }//Beyti
+
+//4-Bir harita (map) yapısı kullanarak ,bir şehirdeki otelleri temsil edin. 
+//Anahtarlar otel adını,değerler ise otelin konumunu ve yıldız sayısını içermelidir. 
+//Kullanıcıya belirli bir otelin konumunu ve yıldız sayısını almak için bir sorgu yapabilen bir işlev tasarlayın .
+//Ayrıca,belirli bir yıldız sayısına sahip tüm otelleri listelemek için bir işlev de ekleyin.-----------------------------------------------------------------------------
 
 // void main() {
-//   Map<String, int> hotel = {
+//   Map<String, int> hotels = {
 //     "Luxes Otel": 5,
 //     "Darwin Otel": 4,
 //     "Nazende Otel": 3,
 //   };
-//   print(hotel["Luxes Otel"]);
+//   print(hotels["Luxes Otel"]);
 // }//5
+
+// void main() {
+//   Map<String, dynamic> hotels ={
+//    'Luxes Hotel':'five','Location':'Osmangazi','Darwin Hotel':'four','Location':'Akpınar','Nazende Hotel':'three','Location':'Fatih'
+//   };
+//   for(var anahtar in hotels.values){
+//   print(anahtar["Nazende Hotel"]);
+//   }
+//  }//Script error
 
